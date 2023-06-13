059c00d49a6eadfcc951e9e4a00eb08238d5bc32
Eduardo <eduaraujp760@gmail.com>
2023-06-13 14:03:04 -0400

    Atividade: Praticando Operadores

---
+++ java.js
@@ -0,0 +1,43 @@
+//Exercício 1
+let num1 = 5
+let num2 = 10
+soma = num1+num2
+console.log(soma)
+
+//Exercício 2
+let pi = 3.14
+let raio = 5
+calculo = pi*raio
+calculo *= 2
+console.log(calculo)
+
+//Exercício 3
+let temperatura = 30
+temperatura *= 9
+temperatura /= 5
+temperatura += 32
+console.log(temperatura)
+
+//Exercício 4
+let nota1 = 7
+let nota2 = 9
+media = nota1+nota2/6
+console.log(media)
+
+//Exercício 5
+let largura = 10
+let comprimento = 20
+retangulo = largura*comprimento
+console.log(retangulo)
+
+//Ecercício 6
+let salarioBruto = 5000
+imposto = 5000/10
+salarioLiquido = salarioBruto-imposto
+console.log(salarioLiquido)
+
+//Ecercício 7
+let horasTrabalhadas = 40
+let valorHora = 50
+salarioBruto = horasTrabalhadas*valorHora
+console.log(salarioBruto)
